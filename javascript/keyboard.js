93a17a75d58a7b44954e9d51646cf398c90e7f77
webspider02 <giorgoszampakikas@gmail.com>
2024-04-06 21:22:18 +0300

    keys part 1

---
+++ javascript/keyboard.js
@@ -0,0 +1,8 @@
+let key;
+const path = document.querySelector("div#wrapper > div#piano-card > div#piano-keyboard"); 
+
+// Creating the keys
+for(let i=0; i<61; i++) {
+    key = document.createElement("div");
+    path.appendChild("key");
+}
